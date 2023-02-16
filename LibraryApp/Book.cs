7437ae3770962433624d1109258c29aff3e43dfd
vanessaburgos <vanessaburgos59@gmail.com>
2023-02-16 21:51:40 +0100

    signalization code

---
+++ LibraryApp/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApp
+{
+    public class Book
+    {
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public int Pages { get; set; }
+        public string Editorial { get; set; }
+
+        public Book(string author, string title, int pages, string editorial)
+        {
+            this.Author = author;
+            this.Title = title;
+            this.Pages = pages;
+            this.Editorial = editorial;
+        }
+
+    }
+}
---
+++ LibraryApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                                  
+            var jsonStringPath = "F:\\ProyectosSolid\\Library\\Library\\LibraryApp\\ShelfContainer\\Quijote.json";
+            var jsonContent = File.ReadAllText(jsonStringPath);
+            var quijote = JsonConvert.DeserializeObject<Book>(jsonContent);
+
+            var bible = new Book("Many Authors", "La Biblia", 1800, "Vaticano");
+
+            var bibleSerialized = JsonConvert.SerializeObject(bible);
+            File.WriteAllText("F:\\ProyectosSolid\\Library\\Library\\LibraryApp\\ShelfContainer\\Bible.json", bibleSerialized);
+        }
+    }
+}
